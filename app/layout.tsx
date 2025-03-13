e00d110d0c4ffc0b177a0f8663c7d5842e830762
Fallen <amir.amon@mail.ru>
2025-03-13 22:34:11 +0900

    Init AI Platform Project

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next"
+import { IBM_Plex_Sans } from "next/font/google"
+import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/sonner"
+import "react-datepicker/dist/react-datepicker.css"
+import "./globals.css"
+import { ClerkProvider } from "@clerk/nextjs"
+
+const IBMPlex = IBM_Plex_Sans({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-ibm-plex"
+})
+
+export const metadata: Metadata = {
+	title: "ProgAI",
+	description: "AI-powered image generator"
+}
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<ClerkProvider
+			appearance={{
+				layout: {
+					socialButtonsVariant: "iconButton",
+					logoImageUrl: "/assets/images/logo-icon.svg"
+				},
+				variables: {
+					colorText: "#fff",
+					colorPrimary: "#624cf5",
+					colorBackground: "#1C1F2E",
+					colorInputBackground: "#252A41",
+					colorInputText: "#fff"
+				}
+			}}
+		>
+			<html
+				lang='en'
+				suppressHydrationWarning
+			>
+				<body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+					{children}
+					<Toaster />
+				</body>
+			</html>
+		</ClerkProvider>
+	)
+}
